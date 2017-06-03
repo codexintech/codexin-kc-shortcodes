e8775ae2840c8d63c1df52f6ba47c82ccf366077
smrafiz <rafiz@codexin.com>
2017-06-03 20:15:42 +0600

    php line added

--- kc_integrated.php
+++ kc_integrated.php
@@ -1,14 +1,12 @@
 <!--  This is zaman -->
 <!-- Added by Rafiz -->
 
-<<<<<<< HEAD
 
 <?php 
 if($i == 1): echo $i;
 endif;
 
  ?>
-=======
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,4 +17,3 @@ endif;
 	jgaskjshdfsjdfhsjdfhsdjfh
 </body>
 </html>
->>>>>>> 96422a3cf3c5a1db1c13bb87097973e154acf958
