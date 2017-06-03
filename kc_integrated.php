96422a3cf3c5a1db1c13bb87097973e154acf958
aktarzaman84@gmail.com <aktarzaman84@gmail.com>
2017-06-03 20:11:23 +0600

    merged zaman aasd

--- kc_integrated.php
+++ kc_integrated.php
@@ -1,2 +1,13 @@
 <!--  This is zaman -->
 <!-- Added by Rafiz -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	jgaskjshdfsjdfhsjdfhsdjfh
+</body>
+</html>
