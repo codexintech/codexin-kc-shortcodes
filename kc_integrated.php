225e269c523725b3a34289117a44e2eaac69706e
aktarzaman84@gmail.com <aktarzaman84@gmail.com>
2017-06-04 01:59:25 +0600

    Duplicate file deleted and unnecessary codes removed from kc_integrated.php

--- kc_integrated.php
+++ kc_integrated.php
@@ -1,19 +1,1 @@
-<!--  This is zaman -->
-<!-- Added by Rafiz -->
 
-
-<?php 
-if($i == 1): echo $i;
-endif;
-
- ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	jgaskjshdfsjdfhsjdfhsdjfh
-</body>
-</html>
